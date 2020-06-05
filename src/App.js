68e267be1a1c1d149987e26ccefe938294f81a48
Kevin Seiter <kevin.seiter@csn.edu>
2020-06-05 15:56:38 -0700

    added material ui NoSsr comonent to lazy load info as needed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HeaderMenu from "./components/HeaderMenu";
 import JumboTron from './components/JumboTron';
 import About from './components/About';
+import NoSsr from '@material-ui/core/NoSsr';
 
 import DATA from './DATA';
 // const DATA = require('./DATA.json');
@@ -9,15 +10,17 @@ import DATA from './DATA';
 function App() {
   return (<div>
     <HeaderMenu />
-    <JumboTron name={DATA.name} title={DATA.JumboTron.title} url={DATA.JumboTron.url} />
-    <About 
-      name={DATA.name} 
-      title={DATA.About.title} 
-      url={DATA.About.url} 
-      social={DATA.About.social}
-      email={DATA.email}
-      cityState={DATA.About.cityState}
-      bodyText={DATA.About.bodyText} />
+    <NoSsr>
+      <JumboTron name={DATA.name} title={DATA.JumboTron.title} url={DATA.JumboTron.url} />
+      <About 
+        name={DATA.name} 
+        title={DATA.About.title} 
+        url={DATA.About.url} 
+        social={DATA.About.social}
+        email={DATA.email}
+        cityState={DATA.About.cityState}
+        bodyText={DATA.About.bodyText} />
+      </NoSsr>
   </div>);
 }
 
